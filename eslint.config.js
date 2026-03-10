818ebabacc1249e00309a89249c91525c0447a37
varspijoyan <varsikpijoyan2003@gmail.com>
2026-03-10 20:45:36 +0400

    Set up Chess game project (React + Typescript)

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...js.configs.recommended.languageOptions.globals,
+        document: "readonly"
+      }
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ]
+    }
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chess Project</h1>
+      <p>React + TypeScript setup is ready.</p>
+    </div>
+  );
+}
+
+export default App;
